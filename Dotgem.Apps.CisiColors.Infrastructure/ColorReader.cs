45177e3a18ce93c3585d68ef84fa91e5dfe36ba3
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-09-15 17:56:53 +0200

    adding a backend to the app

---
+++ Dotgem.Apps.CisiColors.Infrastructure/ColorReader.cs
@@ -0,0 +1,21 @@
+﻿namespace Dotgem.Apps.CisiColors.Infrastructure;
+
+public class ColorReader
+{
+    private readonly string _basePath;
+
+    public ColorReader(string basePath)
+    {
+        _basePath = basePath;
+    }
+
+    public List<ColorDefinition> ReadCisiColors()
+    {
+        return new List<ColorDefinition>();
+    }
+}
+
+public class ColorDefinition
+{
+
+}
