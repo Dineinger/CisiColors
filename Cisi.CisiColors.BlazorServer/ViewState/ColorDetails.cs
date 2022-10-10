c5da26f02d74b1ed1beab0dac4eeab6f889723e3
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-10-10 18:09:32 +0200

    refactoring of ColorDetails

--- Cisi.CisiColors.BlazorServer/ViewState/ColorDetails.cs
+++ Cisi.CisiColors.BlazorServer/ViewState/ColorDetails.cs
@@ -4,24 +4,18 @@ namespace Cisi.CisiColors.BlazorServer.ViewState;
 
 public struct ColorDetails
 {
-    private const int _none = 0b_0000;
-    private const int _hex = 0b_0001;
-    private const int _rgb = 0b_0010;
-    private const int _hsl = 0b_0100;
-    private const int _cmyk = 0b_1000;
-
-    private int _value = _none;
+    private int _value = ColorDetail.Default.Value;
 
     public ColorDetails()
     {
     }
 
-    public bool IsHEX => (_value & _hex) == _hex;
-    public bool IsRGB => (_value & _rgb) == _rgb;
-    public bool IsHSL => (_value & _hsl) == _hsl;
-    public bool IsCMYK => (_value & _cmyk) == _cmyk;
+    public bool IsHexOn => IsOn(ColorDetail.HEX);
+    public bool IsRgbOn => IsOn(ColorDetail.RGB);
+    public bool IsHslOn => IsOn(ColorDetail.HSL);
+    public bool IsCmykOn => IsOn(ColorDetail.CMYK);
 
-    public bool Is(ColorDetail detail)
+    public bool IsOn(ColorDetail detail)
     {
         return (_value & detail.Value) == detail.Value;
     }
@@ -31,9 +25,9 @@ public struct ColorDetails
     public static ColorDetail HSL => ColorDetail.HSL;
     public static ColorDetail CMYK => ColorDetail.CMYK;
 
-    private ColorDetails ToggleValue(ColorDetail value)
+    public ColorDetails ToggleValue(ColorDetail value)
     {
-        if (Is(value))
+        if (IsOn(value))
         {
             _value -= value.Value;
         }
@@ -44,21 +38,9 @@ public struct ColorDetails
         return this;
     }
 
-    public override bool Equals(object? obj)
-    {
-        return obj is ColorDetails details &&
-               Equals(details);
-    }
-
-    public bool Equals(ColorDetails details)
-    {
-        return _value == details._value;
-    }
-
-    public override int GetHashCode()
-    {
-        return _value.GetHashCode();
-    }
+    public override bool Equals(object? obj) => obj is ColorDetails details && Equals(details);
+    public bool Equals(ColorDetails details) => _value == details._value;
+    public override int GetHashCode() => _value.GetHashCode();
 
     public static bool operator ==(ColorDetails left, ColorDetails right) => left.Equals(right);
     public static bool operator !=(ColorDetails left, ColorDetails right) => !(left == right);
@@ -66,24 +48,31 @@ public struct ColorDetails
     public static ColorDetails operator +(ColorDetails left, ColorDetails right) => new() { _value = left._value | right._value };
     public static ColorDetails operator +(ColorDetails left, ColorDetail right) => new() { _value = left._value | right.Value };
     public static ColorDetails operator -(ColorDetails left, ColorDetail right) => new() { _value = left._value - right.Value };
+}
 
-    public sealed class ColorDetail
+public sealed class ColorDetail
+{
+    private const int _default = 0b_0000;
+    private const int _hex = 0b_0001;
+    private const int _rgb = 0b_0010;
+    private const int _hsl = 0b_0100;
+    private const int _cmyk = 0b_1000;
+    private static readonly ColorDetail _defaultDetail = new(_default);
+    private static readonly ColorDetail _hexDetail = new(_hex);
+    private static readonly ColorDetail _rgbDetail = new(_rgb);
+    private static readonly ColorDetail _hslDetail = new(_hsl);
+    private static readonly ColorDetail _cmykDetail = new(_cmyk);
+
+    public static ColorDetail Default => _defaultDetail;
+    public static ColorDetail HEX => _hexDetail;
+    public static ColorDetail RGB => _rgbDetail;
+    public static ColorDetail HSL => _hslDetail;
+    public static ColorDetail CMYK => _cmykDetail;
+
+    public readonly int Value;
+
+    private ColorDetail(int value)
     {
-        private static readonly ColorDetail _hexDetail = new(_hex);
-        private static readonly ColorDetail _rgbDetail = new(_rgb);
-        private static readonly ColorDetail _hslDetail = new(_hsl);
-        private static readonly ColorDetail _cmykDetail = new(_cmyk);
-
-        public readonly int Value;
-
-        public static ColorDetail HEX => _hexDetail;
-        public static ColorDetail RGB => _rgbDetail;
-        public static ColorDetail HSL => _hslDetail;
-        public static ColorDetail CMYK => _cmykDetail;
-
-        private ColorDetail(int value)
-        {
-            Value = value;
-        }
+        Value = value;
     }
 }
